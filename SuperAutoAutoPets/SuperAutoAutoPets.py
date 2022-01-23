49e9d5e2f01328dd93a82fdca62016c802261aff
Blake Davies <bbd@ksu.edu>
2022-01-23 14:47:50 -0600

    I am sick i can't stop

--- SuperAutoAutoPets/SuperAutoAutoPets.py
+++ SuperAutoAutoPets/SuperAutoAutoPets.py
@@ -7,9 +7,200 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
 
+uidict = {
+	'1':(520, 700), #shop1
+	'2':(700,700), #shop2
+	'3':(820, 700), #shop3
+	'4':(950, 700), #shop4
+	'5':(1100, 700), #shop5
 
 
-time.sleep(1)
+	'6':(1250, 700), #shopf2
+	'7':(1400, 700), #shopf1
+
+
+	'a':(530, 450), #team1
+	's':(650, 450), #team2
+	'd':(800, 450), #team3
+	'f':(950,450), #team4
+	'g':(1100, 450), #team5
+
+	'r': (230, 1000), # roll 
+	'w': (1000,1000), # freeze
+	'e': (1600, 1000) # end turn
+		  }
+
+petlistpngs = {
+	"camelcenter.PNG",
+	"camelfrozen.PNG",
+	"crabcenter.PNG",
+	"crabfrozen.PNG",
+	"cricketcenter.PNG",
+	"cricketfrozen.PNG",
+	"deercenter.PNG",
+	"deerfrozen.PNG",
+	"dodocenter.PNG",
+	"dodofrozen.PNG",
+	"dogcenter.PNG",
+	"dogfrozen.PNG",
+	"dolphincenter.PNG",
+	"dolphinfrozen.PNG",
+	"duckcenter.PNG",
+	"duckfrozen.PNG",
+	"elephantcenter.PNG",
+	"elephantfrozen.PNG",
+	"fishcenter.PNG",
+	"fishfrozen.PNG",
+	"flamingocenter.PNG",
+	"flamingofrozen.PNG",
+	"giraffecenter.PNG",
+	"giraffefrozen.PNG",
+	"hedgehogcenter.PNG",
+	"hedgehogfrozen.PNG",
+	"hippocenter.PNG",
+	"hippofrozen.PNG",
+	"horsecenter.PNG",
+	"horsefrozen.PNG",
+	"kangaroocenter.PNG",
+	"kangaroofrozen.PNG",
+	"mosquitocenter.PNG",
+	"mosquitofrozen.PNG",
+	"ottercenter.PNG",
+	"otterfrozen.PNG",
+	"oxcenter.PNG",
+	"oxfrozen.PNG",
+	"peacockcenter.PNG",
+	"peacockfrozen.PNG",
+	"penguincenter.PNG",
+	"penguinfrozen.PNG",
+	"pigcenter.PNG",
+	"pigfrozen.PNG",
+	"ratcenter.PNG",
+	"ratfrozen.PNG",
+	"roostercenter.PNG",
+	"roosterfrozen.PNG",
+	"sheepcenter.PNG",
+	"sheepfrozen.PNG",
+	"shrimpcenter.PNG",
+	"shrimpfrozen.PNG",
+	"skunkcenter.PNG",
+	"skunkfrozen.PNG",
+	"snailcenter.PNG",
+	"snailfrozen.PNG",
+	"spidercenter.PNG",
+	"spiderfrozen.PNG",
+	"swancenter.PNG",
+	"swanfrozen.PNG",
+	"turtlecenter.PNG",
+	"turtlefrozen.PNG",
+	"antcenter.PNG",
+	"antfrozen.PNG",
+	"badgercenter.PNG",
+	"badgerfrozen.PNG",
+	"beavercenter.PNG",
+	"beaverfrozen.PNG",
+	"bisoncenter.PNG",
+	"bisonfrozen.PNG",
+	"blowfishcenter.PNG",
+	"blowfishfrozen.PNG",
+	"bunnycenter.PNG",
+	"bunnyfrozen.PNG",
+	"monkeycenter.PNG",
+	"monkeyfrozen.PNG",
+	"mammothcenter.PNG",
+	"mammothfrozen.png"
+
+	}
+
+foodListPngs = {
+	"meat.PNG",
+	"meatfrozen.PNG",
+	"pear.PNG",
+	"pearfrozen.PNG",
+	"pill.PNG",
+	"pillfrozen.PNG",
+	"salad.PNG",
+	"saladfrozen.PNG",
+	"apple.PNG",
+	"applefrozen.PNG",
+	"can.PNG",
+	"canfrozen.PNG",
+	"cupcake.PNG",
+	"cupcakefrozen.PNG",
+	"garlic.PNG",
+	"garlicfrozen.PNG",
+	"honey.PNG",
+	"honeyfrozen.PNG"
+}
+
+shopBoxesPets = [((475,630),(600, 775)),((605, 630 ),(730, 775)),((750,630),(875, 775)),((900,630),(1025, 775)),((1030,630),(1155, 775))]
+shopBoxesFoods = [((1175,630),(1300, 775)),((1315, 630 ),(1430, 775))]
+
+
+
+time.sleep(3)
+
+#should search the shop space and return the current pets in the shop
+def shopPetsFind():
+	returnlist = ["None unfrozen"] * 5
+
+	for pets in petlistpngs:
+		a = pyautogui.locateAllOnScreen(pets, region=(475, 630, 680, 145), confidence=0.97) 
+
+		if a != None:
+			frozen = False
+			petname = pets
+			if "center.PNG" in petname:
+				petname = petname[:-10]
+			else:
+				petname = petname[:-10]
+				frozen = True
+			for matches in a:
+				locationfound = pyautogui.center(matches)
+				currpostion = 0
+				for rectangles in shopBoxesPets:
+					if pointinrectangle(rectangles[0],rectangles[1], locationfound):
+						if frozen:
+							returnlist[currpostion] = "".join((petname, " ", "frozen"))
+						else:
+							returnlist[currpostion] =  "".join((petname, " ", "unfrozen"))
+					currpostion += 1
+	return returnlist
+
+def pointinrectangle(tl,br,p):
+	if(p[0] > tl[0] and p[0] < br[0] and p[1] < br[1] and p[1] > tl[1]):
+		return True
+	else:
+		return False
+
+
+def shopFoodFind():
+	returnlist = ["None unfrozen"] * 2
+
+	for foods in foodListPngs:
+		print(foods)
+		a = pyautogui.locateAllOnScreen(foods, region=(1175, 630, 350, 145), confidence=0.95) 
+
+		if a != None:
+			frozen = False
+			foodname = foods
+			if "frozen.PNG" in foodname:
+				foodname = foodname[:-10]
+				frozen = True
+			else:
+				foodname = foodname[:-4]
+				
+			for matches in a:
+				locationfound = pyautogui.center(matches)
+				currpostion = 0
+				for rectangles in shopBoxesFoods:
+					if pointinrectangle(rectangles[0],rectangles[1], locationfound):
+						if frozen:
+							returnlist[currpostion] = "".join((foodname, " ", "frozen"))
+						else:
+							returnlist[currpostion] =  "".join((foodname, " ", "unfrozen"))
+					currpostion += 1
+	return returnlist
 
 host = "127.0.0.1"
 port = 9012     
@@ -20,68 +211,62 @@ Server.connect((host, port))
 
 print(Server.recv(1024).decode("utf-8"))
 
+
+
 def findattackandhealthofposisition(position):
 	if position == 0:
-		attack = pyautogui.screenshot("attack.png", region =(486, 490, 40, 40))
-		health = pyautogui.screenshot("health.png", region =(550, 490, 40, 40))
+		attack = pyautogui.screenshot("attack.png", region =(486, 490, 50, 50))
+		health = pyautogui.screenshot("health.png", region =(550, 490, 40, 50))
 	elif position == 1:
-		attack = pyautogui.screenshot("attack.png", region =(621, 490, 40, 40))
-		health = pyautogui.screenshot("health.png", region =(693, 490, 40, 40))	
-
-
-
+		attack = pyautogui.screenshot("attack.png", region =(621, 490, 50, 50))
+		health = pyautogui.screenshot("health.png", region =(693, 490, 40, 50))
+	elif position == 2:
+		attack = pyautogui.screenshot("attack.png", region =(621, 490, 50, 50))
+		health = pyautogui.screenshot("health.png", region =(693, 490, 40, 50))
+	elif position == 3:
+		attack = pyautogui.screenshot("attack.png", region =(621, 490, 50, 50))
+		health = pyautogui.screenshot("health.png", region =(693, 490, 40, 50))
+	elif position == 4:
+		attack = pyautogui.screenshot("attack.png", region =(621, 490, 50, 50))
+		health = pyautogui.screenshot("health.png", region =(693, 490, 40, 50))	
 
 	health, attack = HealthAndAttack()
 	return attack, health
 
 
 def HealthAndAttack():
-		attackimg = cv2.imread("attack.png", cv2.IMREAD_GRAYSCALE)
-		healthimg = cv2.imread("health.png", cv2.IMREAD_GRAYSCALE)
-		attack = pytesseract.image_to_string(attackimg, lang='eng', \
+	attackimg = cv2.imread("attack.png", cv2.IMREAD_GRAYSCALE)
+	healthimg = cv2.imread("health.png", cv2.IMREAD_GRAYSCALE)
+	
+	healthimg = cv2.copyMakeBorder(healthimg, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+	attackimg = cv2.threshold(attackimg, 100, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+	attackimg = cv2.resize(attackimg, (0,0), fx=1.45, fy=1.45) 
+
+	healthimg = cv2.threshold(healthimg, 100, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+	healthimg = cv2.resize(healthimg, (0,0), fx=1.45, fy=1.45)
+
+	attack = pytesseract.image_to_string(attackimg, lang='eng', \
            config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
-		health = pytesseract.image_to_string(healthimg, lang='eng', \
+	health = pytesseract.image_to_string(healthimg, lang='eng', \
            config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
 
-		return health, attack
+	cv2.imwrite("greyhp.png", healthimg)
+	cv2.imwrite("greyattack.png", attackimg)
 
+	if attack == "":
+		attack = 0
+	if health == "":
+		health = 0
+	return health, attack
 
-
-findattackandhealthofposisition(0)
-
-
-
-
-
-
-uidict = {
-	'1':(520, 700), #shop1
-	'2':(700,700), #shop2
-	'3':(820, 700), #shop3
-	'4':(950, 700), #shop4
-	'5':(1100, 700), #shop5
-
-
-	'r':(1250, 700), #shopf2
-	't':(1400, 700), #shopf1
-
-
-	'a':(530, 450), #team1
-	's':(650, 450), #team2
-	'd':(800, 450), #team3
-	'f':(950,450), #team4
-	'g':(1100, 450), #team5
-
-	'q': (230, 1000), # roll 
-	'w': (1000,1000), # freeze
-	'e': (1600, 1000) # end turn
-		  }
 
 def click2spots(spot1, spot2):
 	pyautogui.click(spot1)
 	pyautogui.click(spot2)
 
-
+def click(spot):
+	pyautogui.click(spot)
 
 def clickdic(Keypressed):
 	try:
@@ -90,24 +275,84 @@ def clickdic(Keypressed):
 	except:
 		return False;
 
+def buyslot(shoppos, slotalpha):
+	click2spots(uidict[shoppos], uidict[slotalpha])
 
-def buyslot1():
-	click2spots(uidict['1'], uidict['a'])
+# buy message comes in as BUY shopops, slotAlpha it should only be shoppos slotalpha by the time it reaches here
+def buy(message):
+	buyslot(message[0], message[1])
+	return "PAS"
 
+def freezeunfreeze(slotalpha):
+	click2spots(uidict[slotalpha], uidict['w'])
+
+def endturn():
+	click(uidict['e'])
+	return "PAS"
+
+def reroll():
+	click(uidict['r'])
+	return createSAU()
+
+#messages of type FRE come in "FRE shopLoc"
+def freeze(message):
+	freezeunfreeze(message)
+	return "PAS"
 
 def sendMessage(message):
     Server.send(message.encode("utf-8"))
 
+#SAU should be in format "SAU pet1 f/UF pet2 F/UF pet3 f/UF...
+def createSAU():
+	petShopStrings = shopPetsFind()
+	returnString = "SAU "
+	for petStrings in petShopStrings:
+		returnString = "".join((returnString, " " , petStrings))
+	print(returnString)
+	return returnString
 
-def createCSU():
+#SFU should be in format "SFU food1, food2"
+def createSFU():
+	foodShopStrings = shopFoodFind()
+	returnString = "SFU "
+	for foodstrings in foodShopStrings:
+		returnString = "".join((returnString, " " , foodstrings))
+	print(returnString)
+	return returnString
+	
+
+def createAHU():
 	returnString = "AHU "
+	attack0, health0 = findattackandhealthofposisition(0)
+	attack1, health1 = findattackandhealthofposisition(1)
+	attack2, health2 = findattackandhealthofposisition(2)
+	attack3, health3 = findattackandhealthofposisition(3)
+	attack4, health4 = findattackandhealthofposisition(4)
+	returnString = "".join((returnString, str(attack0), " ", str(health0), " ", str(attack1), " ", str(health1), " ", str(attack2), " ", str(health2), " ", str(attack3), " ", str(health3), " ", str(attack4), " ", str(health4)))
 
 	return returnString
 
 def receivemessage(message):
-    print(message[0:3])
-    if(message[0:3] == "AHU"):
-		sendMessage(createCSU().encode("utf-8"))
+	if message[0:3] == "AHU": #attack health update
+		sendMessage(createAHU())
+	if message[0:3] == "SAU": #Shop Animals Update
+		sendMessage(createSAU())
+	if message[0:3] == "SFU": #Shop Animals Update
+		sendMessage(createSFU())
+	if message[0:3] == "RER": #Reroll
+		sendMessage(reroll())
+	if message[0:3] == "FRE":
+		messageinfo = message.split()
+		sendMessage(freeze(messageinfo[1]))
+	if message[0:3] == "BUY":
+		messageinfo = message.split()
+		sendMessage(buy(messageinfo[:][1:3]))
+	if message[0:3] == "END":
+		sendMessage(endturn())
 
+
+
+sendMessage("connected you are the server")
 
 while True:
+	receivemessage(Server.recv(1024).decode("utf-8"))
--- SuperAutoAutoPetsServer/SuperAutoAutoPetsServer.py
+++ SuperAutoAutoPetsServer/SuperAutoAutoPetsServer.py
@@ -15,13 +15,100 @@ print("the server has been opened")
 
 TeamPets = [["none", 0,0],["none", 0,0],["none", 0,0],["none", 0,0],["none", 0,0]]
 
+#this heuristic while not perfect will mean the ai will generally try and put its strongest units towards the front
+slotweights = [.8],[.95],[.95],[1.0],[1.25]
+
 turn = 0
 
 currGold = 10;
 
-currPetshop = [["none", 0,0],["none", 0,0],["none", 0,0],["none", 0,0],["none", 0,0]]
+currCans = 0
+
+currPetshop = [["none", False],["none", False],["none", False],["none",False],["none", False]]
+
 
 currFoodShop = [["none"],["none"]]
+
+
+
+petListT1 = {
+    "ant" : (2,1),
+    "beaver" : (2,2),
+    "cricket" : (1,2),
+    "duck" : (1,2),
+    "fish" : (2,3),
+    "horse" : (2,1),
+    "mosquito" : (2,2),
+    "otter" : (1,2),
+    "pig" : (3,1)
+    }
+def findT1Value():
+    for pets in petListT1:
+
+
+petListT2 = {
+    "crab" : (3,3),
+    "dodo" : (2,3),
+    "elephant" : (3,5),
+    "flamingo" : (3,1),
+    "hedgehog" : (3,2),
+    "peacock" : (1,5),
+    "rat" : (4,5),
+    "shrimp" : (2,3),
+    "spider" : (2,2),
+    "swan" : (3,3)
+    }
+
+petListT3 = {
+    "badger" : (5,4),
+    "blowfish" : (3,5),
+    "camel" : (2,5),
+    "dog" : (2,2),
+    "giraffe" : (2,5),
+    "kangaroo" : (1,2),
+    "ox" : (1,4),
+    "bunny" : (3,2),
+    "sheep" : (2,2),
+    "snail" : (2,2),
+    "turtle" : (1,2)
+    }
+petListT4 = {
+    "bison" : (6,6),
+    "deer" : (1,1),
+    "dolphin" : (4,6),
+    "hippo" : (4,7),
+    "parrot" : (5,3),
+    "penguin" : (1,2),
+    "rooster" : (5,3),
+    "skunk" : (3,6),
+    "squirrel" : (2,5),
+    "whale" : (2,6),
+    "worm" : (2,2)
+    }
+petListT5 = {
+    "cow" : (4,6),
+    "crocodile" : (8,4),
+    "monkey" : (1,2),
+    "rhino" : (5,8),
+    "scorpion" : (1,1),
+    "seal" : (3,8),
+    "shark" : (4,4),
+    "turkey" : (3,4)
+    }
+petListT6 = {
+    "boar" : (8,6),
+    "cat" : (4,5),
+    "dragon" : (6,8),
+    "fly" : (5,5),
+    "gorilla" : (6,9),
+    "leopard" : (10,4),
+    "mammoth" : (3,10),
+    "snake" : (6,6),
+    "tiger" : (4,3)
+    }
+
+
+
 
 commSocket = None
 # messages received should be in format 
@@ -29,14 +116,46 @@ commSocket = None
 # CSU = current state update 
 # CSU TeamSlot1Name attack Defence, teamslot2name attack Defence,teamslot3name attack Defence, teamslot4name attack Defence, teamslot5name attack Defence
 # AHU = Attack Heath Update 
-# AHU = attack1 heath1, attack 2 health 2, attack 3 health 3, attack 4 health 4, attack 5 health 5
+# AHU attack1 heath1, attack 2 health 2, attack 3 health 3, attack 4 health 4, attack 5 health 5
+# SAU = pet1 frozen/unfrozen pet2 frozen/unfrozen pet3 frozen/unfrozen pet4 frozen/unfrozen pet5 frozen/unfrozen
+# SFU = foodshop1 frozen/unfrozen foodshop2 frozen/unfrozen 
 def receivemessage(message):
-    print(message[0:3])
     if(message[0:3] == "CSU"):
         messageinfo = message.split()
         updateCurrentPets(messageinfo[:][1:16])
     if(message[0:3] == "AHU"):
+        messageinfo = message.split()
+        updateAttackHeath(messageinfo[:][1:11])
+    if(message[0:3] == "SAU"):
+        print(message)
+        messageinfo = message.split()
+        updateShopPets(messageinfo[:][1:11])
+    if(message[0:3] == "SFU"):
+        print(message)
+        messageinfo = message.split()
+        updateFoodShop(messageinfo[:][1:11])
+    if(message[0:3] == "PAS"):
+        pass
 
+
+def updateFoodShop(SFU):
+    currFoodShop[0] = [SFU[0], SFU[1]] 
+    currFoodShop[1] = [SFU[2], SFU[3]] 
+
+def updateShopPets(SAU):
+    currPetshop[0] = [SAU[0], SAU[1]]
+    currPetshop[1] = [SAU[2], SAU[3]]
+    currPetshop[2] = [SAU[4], SAU[5]]
+    currPetshop[3] = [SAU[6], SAU[7]]
+    currPetshop[4] = [SAU[8], SAU[9]]
+
+def updateAttackHeath(ahuString1through10):
+    ahu = ahuString1through10
+    TeamPets[0] = [TeamPets[0][0], ahu[0], ahu[1]]
+    TeamPets[1] = [TeamPets[1][0], ahu[2], ahu[3]]
+    TeamPets[2] = [TeamPets[2][0], ahu[4], ahu[5]]
+    TeamPets[3] = [TeamPets[3][0], ahu[6], ahu[7]]
+    TeamPets[4] = [TeamPets[4][0], ahu[8], ahu[9]]
 
 
 def updateCurrentPets(CSUString1through15):
@@ -53,6 +172,7 @@ def printCurrentTeam():
     for pets in TeamPets:
         print(f"{pets[0]} : {pets[1]} Attack / {pets[2]} Health")
 
+#possible messages BUY, AHU, SAU, CSU, END FRE, RER
 def sendMessage(message):
     commSocket.send(message.encode("utf-8"))
 
@@ -61,11 +181,39 @@ while commSocket == None:
     commSocket, address = server.accept()
 
     print(f"Connected to {address}")
-    sendMessage("connection successfull")
+    sendMessage("connection successfull this is the client")
+
+    print(commSocket.recv(1024).decode("utf-8"))
+
+    sendMessage("SAU")
+
+
+
+    receivemessage(commSocket.recv(1024).decode("utf-8"))
+
+    sendMessage("SFU")
+
+    receivemessage(commSocket.recv(1024).decode("utf-8"))
 
 
 
 
-    printCurrentTeam()
-    commSocket.close()
 
+commSocket.close()
+
+
+
+
+
+
+# the actual ai is below
+
+
+def decidemove():
+    #update the pet shop
+    sendMessage("SAU")
+    receivemessage(commSocket.recv(1024).decode("utf-8"))
+
+    #update the food shop
+    sendMessage("SFU")
+    receivemessage(commSocket.recv(1024).decode("utf-8"))
